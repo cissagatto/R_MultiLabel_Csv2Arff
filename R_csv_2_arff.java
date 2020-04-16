ab1fa87e54be1918be25ccbaf2167be4079225ad
Elaine Cecília Gatto <elainececiliagatto@gmail.com>
2020-04-16 11:50:44 -0300

    Update R_csv_2_arff.java

--- R_csv_2_arff.java
+++ R_csv_2_arff.java
@@ -12,30 +12,18 @@ import weka.core.converters.CSVLoader;
 import weka.filters.*;
 import weka.filters.unsupervised.attribute.NumericToNominal;
 
-/**
- *
- * @author elain
- */
 public class R_csv_2_arff {
 
-    /**
-     * @param args the command line arguments
-     * @throws java.io.IOException
-     */
     public static void main(String[] args) throws IOException, Exception {
         
         String arquivo1 = args[0];
 	String arquivo2 = args[1];
-        String faixa = args[2];        
-        
-        //String arquivo1 = "C:\\Users\\elain\\PartitionsJaccard10Fold\\Partitions\\emotions\\Tr\\Split-1\\particao_2\\grupo_1\\grupo_Tr_1.csv";
-        //String arquivo2 = "C:\\Users\\elain\\PartitionsJaccard10Fold\\Partitions\\emotions\\Tr\\Split-1\\particao_2\\grupo_1\\grupo_Tr_1.arff";        
-        
+        String faixa = args[2];  
+	    
         CSVLoader loader = new CSVLoader();
         loader.setSource(new File(arquivo1));
         
-        Instances data = loader.getDataSet();
-        //System.out.println("\n" + data + "\n");        
+        Instances data = loader.getDataSet();     
         
         data.setClassIndex(data.numAttributes() - 1);        
         NumericToNominal Nominalize = new NumericToNominal();
@@ -46,8 +34,7 @@ public class R_csv_2_arff {
         //nominal
         Nominalize.setOptions(options);
         Nominalize.setInputFormat(data);
-        Instances data1 = Filter.useFilter(data, Nominalize);        
-        //System.out.println("\n" + data1 + "\n");        
+        Instances data1 = Filter.useFilter(data, Nominalize);  
 
         // save ARFF
         ArffSaver saver = new ArffSaver();
